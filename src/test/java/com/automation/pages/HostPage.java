3d35340655ea92d65df80daf831416f9697acb5b
Abhay Krishnan M N(UST <abhaykrishnan333@gmail.com>
2024-08-31 10:47:13 +0530

    Host Preview Changes

--- src/test/java/com/automation/pages/HostPage.java
+++ src/test/java/com/automation/pages/HostPage.java
@@ -8,7 +8,37 @@ public class HostPage extends BasePage{
     @FindBy(xpath = "//button[@aria-label='Host preview']")
     WebElement hostPreview;
 
+    @FindBy(xpath = "//a[@aria-label='Go to Host full profile']")
+    WebElement hostDetails;
+
+    @FindBy(xpath = "//h2[@elementtiming='LCP-target']")
+    WebElement hostName;
+
+    @FindBy(xpath = "//button[@aria-label='Close']")
+    WebElement closeHostDetailsButton;
+
+    @FindBy(xpath = "//img[@fetchpriority='high']")
+    WebElement openCardWithPreview;
+
+
     public void clicksOnHostPreview() {
         hostPreview.click();
+    }
+
+    public void getsHostDetails() {
+        hostDetails.click();
+        switchToNewWindow();
+        javaExecutorGetText(hostName);
+
+    }
+
+    public void closeHostDetails() {
+        switchToNewWindow();
+        closeHostDetailsButton.click();
+    }
+
+    public void openCardWithPreview() {
+        openCardWithPreview.click();
+
     }
 }
--- src/test/java/com/automation/steps/HostSteps.java
+++ src/test/java/com/automation/steps/HostSteps.java
@@ -1,6 +1,8 @@
 package com.automation.steps;
 
 import com.automation.pages.HostPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class HostSteps {
@@ -8,5 +10,20 @@ public class HostSteps {
     @When("user selects a card with host preview")
     public void userSelectsACardWithHostPreview() {
         hostPage.clicksOnHostPreview();
+    }
+
+    @Then("host details are taken")
+    public void hostDetailsAreTaken() {
+        hostPage.getsHostDetails();
+    }
+
+    @When("user close the preview")
+    public void userCloseThePreview() {
+        hostPage.closeHostDetails();
+    }
+
+    @And("user open the same card")
+    public void userOpenTheSameCard() {
+        hostPage.openCardWithPreview();
     }
 }
